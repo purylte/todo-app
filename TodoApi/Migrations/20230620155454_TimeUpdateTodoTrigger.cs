05d2aa8270dc05bc4ea440d9b7f67b5557484121
purylte <purylte@gmail.com>
2023-06-20 22:59:07 +0700

    Add refresh token

---
+++ TodoApi/Migrations/20230620155454_TimeUpdateTodoTrigger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class TimeUpdateTodoTrigger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+CREATE FUNCTION ""Todo_Update_Timestamp_Function""() RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
+BEGIN
+    NEW.""LastUpdated"" := now();
+    RETURN NEW;
+END;
+$$
+");           
+            
+            migrationBuilder.Sql(
+                @"
+CREATE TRIGGER ""UpdateTimestamp""
+    BEFORE INSERT OR UPDATE
+    ON ""Todos""
+    FOR EACH ROW
+    EXECUTE FUNCTION ""Todo_Update_Timestamp_Function""();
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
